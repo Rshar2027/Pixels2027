5ff41e793109f089a347592989f40a7f65939d5d
Gras2027 <155780506+Gras2027@users.noreply.github.com>
2024-02-22 11:09:57 -0500

    Add files via upload

---
+++ teritorial/readme.md
@@ -0,0 +1,33 @@
+<p align="center">
+  <a href="https://mohsenemx.github.io/FXclient/">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="/assets/logo_text_dark.png">
+      <source media="(prefers-color-scheme: light)" srcset="/assets/logo_text_light.png">
+      <img src="/assets/logo_text_light.png" width="20%">
+    </picture>
+  </a>
+</p>
+<p align="center">
+  <a href="https://discord.gg/JEwYWGraj7"><img alt="FX Client Discord" src="https://img.shields.io/discord/1055801912286515220?logo=discord&logoColor=white&label=FX%20Client&color=5865F2"></a>
+  <a href="https://github.com/mohsenemx/FXclient/actions/workflows/deploy_github_pages.yml"><img src="https://github.com/mohsenemx/FXclient/actions/workflows/deploy_github_pages.yml/badge.svg" alt="Build and Publish to GitHub Pages"></a>
+  <a href="https://mohsenemx.github.io/FXclient/"><img src="https://github.com/mohsenemx/FXclient/actions/workflows/pages/pages-build-deployment/badge.svg" alt="pages-build-deployment"></a>
+</p>
+
+## What is FX Client?
+FX Client is the first Territorial.io client, targeting better User Interface and better User Experience, It's basically Territorial.io but better.
+
+### Features:
+1. It's 100% free and open source on Github
+2. It's Ad free and removes game's default ads.
+3. It makes game look cooler, by replacing default assets with new ones.
+4. ~~It makes maximum size for custom maps 4x bigger! (from 4096 x 4096 to 8192 x 8192)~~
+5. Adds a text to display "Troop Density"
+6. Adds a text to display "Maximum Troops"
+7. Adds a win counter
+8. Adds a settings menu which has various options like changing the font name
+9. Adds the ability to view the history of who donated to a player during the game by clicking on their name in the leaderboard
+
+### Contact Us
+Our Website: https://mohsenemx.github.io/FXclient/
+
+Our Discord: https://discord.gg/JEwYWGraj7
---
+++ teritorial/static/fx_core.js
@@ -0,0 +1,146 @@
+const fx_version = '0.6.0.3'; // FX Client Version
+const fx_update = 'Feb 15'; // FX Client Last Updated
+
+
+if (localStorage.getItem("fx_winCount") == undefined || localStorage.getItem("fx_winCount") == null) {
+    var wins_counter = 0;
+    console.log('Couldn\'t find a saved win data. creating one...');
+} else if (localStorage.getItem("fx_winCount") != undefined || localStorage.getItem("fx_winCount") != null) {
+    var wins_counter = localStorage.getItem("fx_winCount");
+}
+
+var settings = {
+    "fontName": "Trebuchet MS",
+    "showBotDonations": false,
+    "hideAllLinks": false,
+    "realisticNames": false,
+    //"customMapFileBtn": true
+};
+var settingsManager = new (function() {
+    var inputFields = {
+        fontName: document.getElementById("settings_fontname")
+    };
+    var checkboxFields = {
+        //showBotDonations: document.getElementById("settings_donations_bots"),
+        hideAllLinks: document.getElementById("settings_hidealllinks"),
+        realisticNames: document.getElementById("settings_realisticnames"),
+        //customMapFileBtn: document.getElementById("settings_custommapfileinput")
+    };
+    this.save = function() {
+        Object.keys(inputFields).forEach(function(key) { settings[key] = inputFields[key].value.trim(); });
+        Object.keys(checkboxFields).forEach(function(key) { settings[key] = checkboxFields[key].checked; });
+        this.applySettings();
+        WindowManager.closeWindow("settings");
+        localStorage.setItem("fx_settings", JSON.stringify(settings));
+        // should probably firgure out a way to do this without reloading - // You can't do it, localstorages REQUIRE you to reload
+        window.location.reload();
+    };
+    this.syncFields = function() {
+        Object.keys(inputFields).forEach(function(key) { inputFields[key].value = settings[key]; });
+        Object.keys(checkboxFields).forEach(function(key) { checkboxFields[key].checked = settings[key]; });
+    };
+    this.resetAll = function() {
+        if (!confirm("Are you Really SURE you want to RESET ALL SETTINGS back to the default?")) return;
+        localStorage.removeItem("fx_settings");
+        window.location.reload();
+    };
+    this.applySettings = function() {
+        //setVarByName("bu", "px " + settings.fontName);
+    };
+});
+function removeWins() {
+    var confirm1 = confirm('Do you really want to reset your Wins?');
+    if (confirm1) {
+        wins_counter = 0;
+        localStorage.removeItem('fx_winCount');
+        alert("Successfully reset wins");
+    }
+}
+var WindowManager = new (function() {
+    var windows = {};
+    this.add = function(newWindow) {
+        windows[newWindow.name] = newWindow;
+        windows[newWindow.name].isOpen = false;
+    };
+    this.openWindow = function(windowName, ...args) {
+        if (windows[windowName].isOpen === true) return;
+        if (windows[windowName].beforeOpen !== undefined) windows[windowName].beforeOpen(...args);
+        windows[windowName].isOpen = true;
+        windows[windowName].element.style.display = null;
+    };
+    this.closeWindow = function(windowName) {
+        if (windows[windowName].isOpen === false) return;
+        windows[windowName].isOpen = false;
+        windows[windowName].element.style.display = "none";
+    };
+    this.closeAll = function() {
+        Object.values(windows).forEach(function(windowObj) {
+            WindowManager.closeWindow(windowObj.name);
+        });
+    };
+});
+WindowManager.add({
+    name: "settings",
+    element: document.querySelector(".settings"),
+    beforeOpen: function() { settingsManager.syncFields(); }
+});
+WindowManager.add({
+    name: "donationHistory",
+    element: document.querySelector("#donationhistory"),
+    beforeOpen: function(isSingleplayer) {
+        document.getElementById("donationhistory_note").style.display = ((true || settings.showBotDonations || /*getVarByName("dt")*/ isSingleplayer) ? "none" : "block");
+    }
+});
+document.getElementById("canvasA").addEventListener("mousedown", WindowManager.closeAll);
+document.getElementById("canvasA").addEventListener("touchstart", WindowManager.closeAll);
+var settingsGearIcon = document.createElement('img');
+settingsGearIcon.setAttribute('src', 'geari_white.png');
+
+var donationsTracker = new (function(){
+    this.donationHistory = Array(512);
+    // fill the array with empty arrays with length of 3
+    //for (var i = 0; i < 512; i++) this.donationHistory.push([]); // not needed as .reset is called on game start
+    // from inside of game:
+    // ((!gE[g].startsWith("[Bot] ") || settings.showBotDonations) && donationsTracker.logDonation(g,k,x))
+    this.logDonation = function(senderID, receiverID, amount) {
+        const donationInfo = [senderID, receiverID, amount];
+        this.donationHistory[receiverID].push(donationInfo);
+        this.donationHistory[senderID].push(donationInfo);
+    };
+    this.getRecipientHistoryOf = function(playerID) {
+        return this.donationHistory[playerID];
+    };
+    this.reset = function() { for (var i = 0; i < 512; i++) this.donationHistory[i] = []; };
+});
+// usage from inside: displayDonationsHistory(Y, gE);
+function displayDonationsHistory(playerID, playerNames, isSingleplayer) {
+    var history = donationsTracker.getRecipientHistoryOf(playerID);
+    console.log("History for " + playerNames[playerID] + ":");
+    console.log(history);
+    document.querySelector("#donationhistory h1").innerHTML = "Donation history for " + playerNames[playerID];
+    var historyText = "";
+    history.reverse();
+    if (history.length > 0) history.forEach(function(historyItem, index) {
+        historyText += `<span class="color-light-gray">${(history.length - index)}.</span> `;
+        if (playerID === historyItem[1])
+            historyText += `Received <span class="color-green">${historyItem[2]}</span> resources from ${playerNames[historyItem[0]]}<br>`;
+        else historyText += `Sent <span class="color-red">${historyItem[2]}</span> resources to ${playerNames[historyItem[1]]}<br>`;
+    });
+    else historyText = "Nothing to display";
+    document.querySelector("#donationhistory p#donationhistory_text").innerHTML = historyText;
+    WindowManager.openWindow("donationHistory", isSingleplayer);
+}
+
+var utils = new (function() {
+    this.getMaxTroops = function(playerTerritories, playerID) { return (playerTerritories[playerID]*150).toString(); };
+    this.getDensity = function(playerBalances, playerTerritories, playerID) {
+        return (Math.floor((playerBalances[playerID] / ((playerTerritories[playerID] === 0 ? 1 : playerTerritories[playerID]) * 150)) * 100) + "%");
+    };
+});
+
+if (localStorage.getItem("fx_settings") !== null) {
+    settings = {...settings, ...JSON.parse(localStorage.getItem("fx_settings"))};
+}
+//settingsManager.applySettings();
+
+console.log('Successfully loaded FX Client');
